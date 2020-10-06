143efe835f68225b1c0e7566bee2012bc3a2a39c
pgrebenyuk <grebenyuk.java@gmail.com>
2020-10-06 12:12:11 +0300

    created dao

---
+++ src/main/java/Const.java
@@ -0,0 +1,8 @@
+public class Const {
+    public static final String TABLE_ARTICLE = "articles";
+    public static final String ID_ARTICLES = "id_articles";
+    public static final String PRICE = "price";
+    public static final String TABLE_MANUFACTURERS = "manufacturers";
+    public static final String ID_MANUFACTURER = "id_manufacturer";
+    public static final String MANUFACTURER = "manufacturer";
+}
