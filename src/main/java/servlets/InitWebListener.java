1a5219ab287b4d57e5bb196b23911747d85f6fcd
pgrebenyuk <grebenyuk.java@gmail.com>
2020-10-15 14:48:06 +0300

    deleted DataBaseConnectionManager

--- src/main/java/servlets/InitWebListener.java
+++ src/main/java/servlets/InitWebListener.java
@@ -1,9 +1,8 @@
-package base.servlets;
+package servlets;
 
 import base.configs.MyConfig;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import servlets.ArticlesServlet;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
@@ -11,23 +10,23 @@ import javax.servlet.ServletContextListener;
 import javax.servlet.ServletRegistration;
 import javax.servlet.annotation.WebListener;
 
-
-//це новий клас, йоо задача зробити щось перед тим, як створяться сервлети
-// і тут я хочу показати, як я підміню 1 сервлет
-// я заставлю сервлет апі не самому створити сервлет, а використати мій об*єкт
-// і тут же я і зроблю контекст спрінга
 @WebListener
 public class InitWebListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
-        //дістаю сервлет з спрінга
-        ArticlesServlet articlesServlet = context.getBean(ArticlesServlet.class);
 
         ServletContext servletContext = servletContextEvent.getServletContext();
-        //реєструю цей сервлет
-        ServletRegistration servletRegistration = servletContext.addServlet("articles", articlesServlet);
-        servletRegistration.addMapping("/articles");
+        ServletRegistration servletRegistrationArticles = servletContext
+                .addServlet("articles", context.getBean(ArticlesServlet.class));
+        servletRegistrationArticles.addMapping("/articles");
+        ServletRegistration servletRegistrationArticle = servletContext
+                .addServlet("article", context.getBean(ArticleServlet.class));
+        servletRegistrationArticle.addMapping("/article");
+        ServletRegistration servletRegistrationCreateArticle = servletContext
+                .addServlet("createArticle", context.getBean(CreateArticleServlet.class));
+        servletRegistrationCreateArticle.addMapping("/create-article");
+
     }
 
     @Override
